3f6d80b1610cbe4e96b9a190e600cff2a2234d29
Arye Eidelman <aryedov.e@gmail.com>
2026-01-03 21:02:18 -0500

    Remove craco and update to React Scripts 5.0.1; migrate Tailwind CSS setup to postcss.config.js and update dependencies.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html'
+  ],
   theme: {
     extend: {
       gridTemplateColumns: {
@@ -17,9 +19,6 @@ module.exports = {
         'lanscape': {'raw': '(orientation: lanscape)'}
       }
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
